57822446477674c67519b1f96d93cb4a4f93300e
NLou-Ulaval <nickolas.lou.1@ulaval.ca>
2025-06-17 10:47:29 -0400

    wip

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/emotionGen/', // GitHub repo name
+  base: '/descGen/', // GitHub repo name
   plugins: [react()],
 });
