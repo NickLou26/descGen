d0e5c4bee83b5d219621d0f260e3848f1c9cdc15
NLou-Ulaval <nickolas.lou.1@ulaval.ca>
2025-06-25 18:07:26 -0400

    added presets

--- src/App.jsx
+++ src/App.jsx
@@ -87,6 +87,17 @@ const emotions = [
   },
 ];
 
+let preset1 = '',
+  preset2 = '',
+  preset3 = '',
+  preset4 = '',
+  preset5 = '',
+  preset6 = '',
+  preset7 = '',
+  preset8 = '',
+  preset9 = '',
+  preset10 = '';
+
 function Dropdown({ label, options, selected, onChange }) {
   const handleChange = (e) => {
     const value = e.target.value;
@@ -241,8 +252,14 @@ export default function ScrollSelections() {
   }, [selections]);
 
   //Copy output to clipboard
-  const handleCopy = () => {
+  const handleCopyTemplate = () => {
     navigator.clipboard.writeText(generateOutput());
+    handleReset();
+  };
+
+  //Copy presets to clipboard
+  const handleCopyPreset = (value) => {
+    navigator.clipboard.writeText(value);
     handleReset();
   };
 
@@ -265,9 +282,9 @@ export default function ScrollSelections() {
   };
 
   //Save template in cookies
-  const saveCookie = () => {
-    Cookies.set('templateValue', template, { expires: 365 }); // Cookie expires in 7 days
-    alert('Template saved!');
+  const saveCookie = (cookieName, value) => {
+    Cookies.set(cookieName, value, { expires: 365 }); // Cookie expires in 7 days
+    alert(`Saved!`);
   };
 
   //Save template in cookies
@@ -342,7 +359,7 @@ export default function ScrollSelections() {
           }}
         />
         <button
-          onClick={saveCookie}
+          onClick={() => saveCookie('templateValue', template)}
           style={{
             padding: '0.75rem 1.5rem',
             fontSize: '1rem',
@@ -397,7 +414,7 @@ export default function ScrollSelections() {
       >
         <button
           disabled={emotionSelection == ''}
-          onClick={handleCopy}
+          onClick={handleCopyTemplate}
           style={{
             padding: '0.5rem 1rem',
             backgroundColor: emotionSelection == '' ? '#1a1a1a' : '#4CAF50',
@@ -434,6 +451,520 @@ export default function ScrollSelections() {
         </button>
       </div>
       <p style={{ color: '#aaa' }}>Copies "Foreign Language" to clipboard</p>
+
+      <h2
+        style={{
+          textAlign: 'center',
+          fontSize: '1.5rem',
+          fontWeight: 'bold',
+          marginBottom: '2rem',
+        }}
+      >
+        Presets
+      </h2>
+      <p style={{ textAlign: 'center', color: '#aaa', marginTop: '2rem' }}>
+        For most used words/templates
+      </p>
+      {/* Preset 1 */}
+      <div
+        style={{ marginTop: '1rem', display: 'flex', justifyContent: 'center' }}
+      >
+        <input
+          type="text"
+          defaultValue={Cookies.get('preset1')}
+          onChange={(e) => {
+            preset1 = e.target.value;
+          }}
+          style={{
+            width: '80%',
+            padding: '0.75rem',
+            fontSize: '1rem',
+            backgroundColor: '#2d2d2d',
+            color: 'white',
+            border: '1px solid #444',
+            borderRadius: '8px',
+            marginRight: '1rem',
+          }}
+        />
+        <button
+          onClick={() => saveCookie('preset1', preset1)}
+          style={{
+            padding: '0.75rem 1.5rem',
+            fontSize: '1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '8px',
+            cursor: 'pointer',
+          }}
+        >
+          Save
+        </button>
+        <button
+          onClick={() => handleCopyPreset(preset1)}
+          style={{
+            marginLeft: '1rem',
+            padding: '0.5rem 1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '6px',
+            cursor: 'pointer',
+          }}
+        >
+          Copy
+        </button>
+      </div>
+      {/* Preset 2 */}
+      <div
+        style={{ marginTop: '1rem', display: 'flex', justifyContent: 'center' }}
+      >
+        <input
+          type="text"
+          defaultValue={Cookies.get('preset2')}
+          onChange={(e) => {
+            preset2 = e.target.value;
+          }}
+          style={{
+            width: '80%',
+            padding: '0.75rem',
+            fontSize: '1rem',
+            backgroundColor: '#2d2d2d',
+            color: 'white',
+            border: '1px solid #444',
+            borderRadius: '8px',
+            marginRight: '1rem',
+          }}
+        />
+        <button
+          onClick={() => saveCookie('preset2', preset2)}
+          style={{
+            padding: '0.75rem 1.5rem',
+            fontSize: '1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '8px',
+            cursor: 'pointer',
+          }}
+        >
+          Save
+        </button>
+        <button
+          onClick={() => handleCopyPreset(preset2)}
+          style={{
+            marginLeft: '1rem',
+            padding: '0.5rem 1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '6px',
+            cursor: 'pointer',
+          }}
+        >
+          Copy
+        </button>
+      </div>
+      {/* Preset 3 */}
+      <div
+        style={{ marginTop: '1rem', display: 'flex', justifyContent: 'center' }}
+      >
+        <input
+          type="text"
+          defaultValue={Cookies.get('preset3')}
+          onChange={(e) => {
+            preset3 = e.target.value;
+          }}
+          style={{
+            width: '80%',
+            padding: '0.75rem',
+            fontSize: '1rem',
+            backgroundColor: '#2d2d2d',
+            color: 'white',
+            border: '1px solid #444',
+            borderRadius: '8px',
+            marginRight: '1rem',
+          }}
+        />
+        <button
+          onClick={() => saveCookie('preset3', preset3)}
+          style={{
+            padding: '0.75rem 1.5rem',
+            fontSize: '1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '8px',
+            cursor: 'pointer',
+          }}
+        >
+          Save
+        </button>
+        <button
+          onClick={() => handleCopyPreset(preset3)}
+          style={{
+            marginLeft: '1rem',
+            padding: '0.5rem 1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '6px',
+            cursor: 'pointer',
+          }}
+        >
+          Copy
+        </button>
+      </div>
+      {/* Preset 4 */}
+      <div
+        style={{ marginTop: '1rem', display: 'flex', justifyContent: 'center' }}
+      >
+        <input
+          type="text"
+          defaultValue={Cookies.get('preset4')}
+          onChange={(e) => {
+            preset4 = e.target.value;
+          }}
+          style={{
+            width: '80%',
+            padding: '0.75rem',
+            fontSize: '1rem',
+            backgroundColor: '#2d2d2d',
+            color: 'white',
+            border: '1px solid #444',
+            borderRadius: '8px',
+            marginRight: '1rem',
+          }}
+        />
+        <button
+          onClick={() => saveCookie('preset4', preset4)}
+          style={{
+            padding: '0.75rem 1.5rem',
+            fontSize: '1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '8px',
+            cursor: 'pointer',
+          }}
+        >
+          Save
+        </button>
+        <button
+          onClick={() => handleCopyPreset(preset4)}
+          style={{
+            marginLeft: '1rem',
+            padding: '0.5rem 1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '6px',
+            cursor: 'pointer',
+          }}
+        >
+          Copy
+        </button>
+      </div>
+      {/* Preset 5 */}
+      <div
+        style={{ marginTop: '1rem', display: 'flex', justifyContent: 'center' }}
+      >
+        <input
+          type="text"
+          defaultValue={Cookies.get('preset5')}
+          onChange={(e) => {
+            preset5 = e.target.value;
+          }}
+          style={{
+            width: '80%',
+            padding: '0.75rem',
+            fontSize: '1rem',
+            backgroundColor: '#2d2d2d',
+            color: 'white',
+            border: '1px solid #444',
+            borderRadius: '8px',
+            marginRight: '1rem',
+          }}
+        />
+        <button
+          onClick={() => saveCookie('preset5', preset5)}
+          style={{
+            padding: '0.75rem 1.5rem',
+            fontSize: '1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '8px',
+            cursor: 'pointer',
+          }}
+        >
+          Save
+        </button>
+        <button
+          onClick={() => handleCopyPreset(preset5)}
+          style={{
+            marginLeft: '1rem',
+            padding: '0.5rem 1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '6px',
+            cursor: 'pointer',
+          }}
+        >
+          Copy
+        </button>
+      </div>
+      {/* Preset 6 */}
+      <div
+        style={{ marginTop: '1rem', display: 'flex', justifyContent: 'center' }}
+      >
+        <input
+          type="text"
+          defaultValue={Cookies.get('preset6')}
+          onChange={(e) => {
+            preset6 = e.target.value;
+          }}
+          style={{
+            width: '80%',
+            padding: '0.75rem',
+            fontSize: '1rem',
+            backgroundColor: '#2d2d2d',
+            color: 'white',
+            border: '1px solid #444',
+            borderRadius: '8px',
+            marginRight: '1rem',
+          }}
+        />
+        <button
+          onClick={() => saveCookie('preset6', preset6)}
+          style={{
+            padding: '0.75rem 1.5rem',
+            fontSize: '1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '8px',
+            cursor: 'pointer',
+          }}
+        >
+          Save
+        </button>
+        <button
+          onClick={() => handleCopyPreset(preset6)}
+          style={{
+            marginLeft: '1rem',
+            padding: '0.5rem 1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '6px',
+            cursor: 'pointer',
+          }}
+        >
+          Copy
+        </button>
+      </div>
+      {/* Preset 7 */}
+      <div
+        style={{ marginTop: '1rem', display: 'flex', justifyContent: 'center' }}
+      >
+        <input
+          type="text"
+          defaultValue={Cookies.get('preset7')}
+          onChange={(e) => {
+            preset7 = e.target.value;
+          }}
+          style={{
+            width: '80%',
+            padding: '0.75rem',
+            fontSize: '1rem',
+            backgroundColor: '#2d2d2d',
+            color: 'white',
+            border: '1px solid #444',
+            borderRadius: '8px',
+            marginRight: '1rem',
+          }}
+        />
+        <button
+          onClick={() => saveCookie('preset7', preset7)}
+          style={{
+            padding: '0.75rem 1.5rem',
+            fontSize: '1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '8px',
+            cursor: 'pointer',
+          }}
+        >
+          Save
+        </button>
+        <button
+          onClick={() => handleCopyPreset(preset7)}
+          style={{
+            marginLeft: '1rem',
+            padding: '0.5rem 1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '6px',
+            cursor: 'pointer',
+          }}
+        >
+          Copy
+        </button>
+      </div>
+      {/* Preset 8 */}
+      <div
+        style={{ marginTop: '1rem', display: 'flex', justifyContent: 'center' }}
+      >
+        <input
+          type="text"
+          defaultValue={Cookies.get('preset8')}
+          onChange={(e) => {
+            preset8 = e.target.value;
+          }}
+          style={{
+            width: '80%',
+            padding: '0.75rem',
+            fontSize: '1rem',
+            backgroundColor: '#2d2d2d',
+            color: 'white',
+            border: '1px solid #444',
+            borderRadius: '8px',
+            marginRight: '1rem',
+          }}
+        />
+        <button
+          onClick={() => saveCookie('preset8', preset8)}
+          style={{
+            padding: '0.75rem 1.5rem',
+            fontSize: '1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '8px',
+            cursor: 'pointer',
+          }}
+        >
+          Save
+        </button>
+        <button
+          onClick={() => handleCopyPreset(preset8)}
+          style={{
+            marginLeft: '1rem',
+            padding: '0.5rem 1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '6px',
+            cursor: 'pointer',
+          }}
+        >
+          Copy
+        </button>
+      </div>
+      {/* Preset 9 */}
+      <div
+        style={{ marginTop: '1rem', display: 'flex', justifyContent: 'center' }}
+      >
+        <input
+          type="text"
+          defaultValue={Cookies.get('preset9')}
+          onChange={(e) => {
+            preset9 = e.target.value;
+          }}
+          style={{
+            width: '80%',
+            padding: '0.75rem',
+            fontSize: '1rem',
+            backgroundColor: '#2d2d2d',
+            color: 'white',
+            border: '1px solid #444',
+            borderRadius: '8px',
+            marginRight: '1rem',
+          }}
+        />
+        <button
+          onClick={() => saveCookie('preset9', preset9)}
+          style={{
+            padding: '0.75rem 1.5rem',
+            fontSize: '1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '8px',
+            cursor: 'pointer',
+          }}
+        >
+          Save
+        </button>
+        <button
+          onClick={() => handleCopyPreset(preset9)}
+          style={{
+            marginLeft: '1rem',
+            padding: '0.5rem 1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '6px',
+            cursor: 'pointer',
+          }}
+        >
+          Copy
+        </button>
+      </div>
+      {/* Preset 10 */}
+      <div
+        style={{ marginTop: '1rem', display: 'flex', justifyContent: 'center' }}
+      >
+        <input
+          type="text"
+          defaultValue={Cookies.get('preset10')}
+          onChange={(e) => {
+            preset10 = e.target.value;
+          }}
+          style={{
+            width: '80%',
+            padding: '0.75rem',
+            fontSize: '1rem',
+            backgroundColor: '#2d2d2d',
+            color: 'white',
+            border: '1px solid #444',
+            borderRadius: '8px',
+            marginRight: '1rem',
+          }}
+        />
+        <button
+          onClick={() => saveCookie('preset10', preset10)}
+          style={{
+            padding: '0.75rem 1.5rem',
+            fontSize: '1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '8px',
+            cursor: 'pointer',
+          }}
+        >
+          Save
+        </button>
+        <button
+          onClick={() => handleCopyPreset(preset10)}
+          style={{
+            marginLeft: '1rem',
+            padding: '0.5rem 1rem',
+            backgroundColor: '#444',
+            color: 'white',
+            border: 'none',
+            borderRadius: '6px',
+            cursor: 'pointer',
+          }}
+        >
+          Copy
+        </button>
+      </div>
     </div>
   );
 }
